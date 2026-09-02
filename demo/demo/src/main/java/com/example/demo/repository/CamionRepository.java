54f34c4516af83ea871d293f8db4e56b505113c8
juanpabloburgos <167448713+juanpabloburgos@users.noreply.github.com>
2026-09-02 12:15:25 -0500

    Implementar autenticacion JWT

--- demo/demo/src/main/java/com/example/demo/repository/CamionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Camion;
-
-public interface CamionRepository extends JpaRepository<Camion, Long> {
-}
--- demo/demo/src/main/java/com/example/demo/repository/ConductorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Conductor;
-
-public interface ConductorRepository extends JpaRepository<Conductor, Long> {
-}
